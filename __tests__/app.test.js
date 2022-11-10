f39c69c5a808490ac0998be10db7e82a14293e00
Sam Adams <sam.adams7785@gmail.com>
2022-11-10 12:01:46 -0800

    GET apps list test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,12 +11,92 @@ describe('backend-express-template routes', () => {
   it('GET /apps should return a list of apps', async () => {
     const res = await request(app).get('/apps');
     expect(res.status).toBe(200);
-    expect(res.body[0]).toEqual({
-      app: expect.any(String),
-      version: expect.any(String),
-      bundleid: expect.any(String),
-      filename: expect.any(String),
-    });
+    expect(res.body).toEqual([
+      {
+        id: '1',
+        app: 'Alpha',
+        version: '7.3',
+        bundleid: 'gov.ftc.Y-Solowarm',
+        filename: 'MattisEgestas.ppt',
+      },
+      {
+        id: '2',
+        app: 'Bytecard',
+        version: '6.24',
+        bundleid: 'com.blogtalkradio.Daltfresh',
+        filename: 'Sapien.avi',
+      },
+      {
+        id: '3',
+        app: 'Tresom',
+        version: '6.9.0',
+        bundleid: 'org.mozilla.Regrant',
+        filename: 'Ornare.avi',
+      },
+      {
+        id: '4',
+        app: 'Opela',
+        version: '7.64',
+        bundleid: 'com.netscape.Toughjoyfax',
+        filename: 'Eget.jpeg',
+      },
+      {
+        id: '5',
+        app: 'Zaam-Dox',
+        version: '8.88',
+        bundleid: 'com.ihg.Zontrax',
+        filename: 'BlanditUltricesEnim.mov',
+      },
+      {
+        id: '6',
+        app: 'Bytecard',
+        version: '9.0',
+        bundleid: 'com.netvibes.Holdlamis',
+        filename: 'UrnaUtTellus.mp3',
+      },
+      {
+        id: '7',
+        app: 'Voyatouch',
+        version: '9.4',
+        bundleid: 'org.slashdot.Veribet',
+        filename: 'Posuere.avi',
+      },
+      {
+        id: '8',
+        app: 'Veribet',
+        version: '0.64',
+        bundleid: 'com.github.Treeflex',
+        filename: 'VivamusInFelis.mp3',
+      },
+      {
+        id: '9',
+        app: 'Temp',
+        version: '0.8.0',
+        bundleid: 'com.wikia.Sub-Ex',
+        filename: 'Sociis.ppt',
+      },
+      {
+        id: '10',
+        app: 'Tresom',
+        version: '2.9.3',
+        bundleid: 'jp.ameblo.Cardify',
+        filename: 'LacusCurabiturAt.xls',
+      },
+      {
+        id: '11',
+        app: 'Hatity',
+        version: '3.6.7',
+        bundleid: 'com.parallels.Sonair',
+        filename: 'AcConsequat.gif',
+      },
+      {
+        id: '12',
+        app: 'Tin',
+        version: '0.20',
+        bundleid: 'pl.google.Zoolab',
+        filename: 'VelitVivamus.mp3',
+      },
+    ]);
   });
 
   afterAll(() => {
